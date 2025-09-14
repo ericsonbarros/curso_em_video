d5bb05194330108b4aa0fe1f1e2280c9897a98a1
ericsonbarros <ericsonengwork@gmail.com>
2025-09-13 23:44:25 -0300

    Delete ex.13.py

--- ex.13.py
+++
@@ -1,8 +0,0 @@
-import math
-an = float(input('Digite o valor do angulo:'))
-sen = math.sin(math.radians(an))
-cos = math.cos(math.radians(an))
-tan = math.tan(math.radians(an))
-print('Sendo o angulo {}, o cosseno é {:.2f}, o seno é {:.2f} e a tangente corresponde a {:.2f}'.format(an, cos, sen, tan))
-
-
