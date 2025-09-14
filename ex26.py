c22aae2867eced49fe6ef87cdb98a1222352b961
ericsonbarros <ericsonengwork@gmail.com>
2025-09-13 23:46:45 -0300

    Delete ex26.py

--- ex26.py
+++
@@ -1,5 +0,0 @@
-distancia = float(input('Quantos km foi percorrido:'))
-if distancia <= 200:
-    print('O valor a ser cobrado e de 0,50 por km percorrido, totalizando {}'.format(distancia*0.50))
-else:
-    print('O valor a ser cobrado e de 0,45 por km percorrido, totalizando {}'.format(distancia*0.45))
