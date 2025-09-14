7e1eef413a56621b03ba92d621ef7324467cf142
ericsonbarros <ericsonengwork@gmail.com>
2025-09-13 23:42:33 -0300

    Delete ex.07.py

--- ex.07.py
+++
@@ -1,5 +0,0 @@
-variavel = float(input('Digite o valor escolhido em metros:'))
-cm = variavel*100
-mm = variavel*1000
-print('o valor de {} corresponde a {} centimetros e {} milimetros.'.format(variavel, cm, mm))
-
