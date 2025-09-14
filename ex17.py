09bdedf1108d99c21fd67eab5dfed82c005bf8ac
ericsonbarros <ericsonengwork@gmail.com>
2025-09-13 23:46:32 -0300

    Delete ex17.py

--- ex17.py
+++
@@ -1,7 +0,0 @@
-name = str(input('Digite seu nome:')).strip()
-print('Analisando seu nome...')
-print('Seu nome com letras maiusculas é {}'.format(name.upper()))
-print('Seu nome em letras minusculas é {}'.format(name.lower()))
-print('Seu nome tem ao todo {} letras'.format(len(name) - name.count(' ')))
-print('Seu primeiro nome tem {} letras'.format(name.find(' ')))
-
