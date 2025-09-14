bdaf8db2d719a5587bb734594c11149fae8b37dc
ericsonbarros <ericsonengwork@gmail.com>
2025-09-13 23:46:24 -0300

    Delete ex002.py

--- ex002.py
+++
@@ -1,6 +0,0 @@
-n1 = int(input('Digite o primeiro numero'))
-n2 = int(input('Digite o segundo numero'))
-s = n1 + n2
-# print('A soma entre', n1, '+', n2, 'corresponde a {}'.format(s))
-print('A soma entre {} + {} corresponde a {}'.format(n1, n2, s))
-
